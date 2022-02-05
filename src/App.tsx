c197bbbcd5aa331e5bdd73fefa69eb7bb8d01f64
Albert Gabdullin <agabdullin@dev.vtb.ru>
2022-02-05 23:37:35 +0300

    fix router path

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
 import React from 'react';
 import { ApolloProvider } from '@apollo/client';
-import { Routes, Route, HashRouter } from 'react-router-dom';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import { BaseStyles, ThemeProvider } from '@primer/react';
 import apolloClient from './core/ApolloClient';
 import MainPage from './pages';
 import UserPage from './pages/UserPage';
 
 const App = () => (
-  <HashRouter basename="/">
+  <BrowserRouter>
     <ThemeProvider>
       <BaseStyles>
         <ApolloProvider client={apolloClient}>
           <Routes>
             <Route path="/" element={<MainPage />} />
-            <Route path="/user/:id" element={<UserPage />} />
+            <Route path="users/:id" element={<UserPage />} />
           </Routes>
         </ApolloProvider>
       </BaseStyles>
     </ThemeProvider>
-  </HashRouter>
+  </BrowserRouter>
 );
 
 export default App;
