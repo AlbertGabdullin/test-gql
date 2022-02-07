3ba4a132cda781f224653524d12f9dde0d289452
Albert Gabdullin <agabdullin@dev.vtb.ru>
2022-02-07 13:58:09 +0300

    fix main page text

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const MainPage = () => {
-  return <div>this is main page</div>;
+  return (
+    <div>
+      This is Main page. <Link to="/users/gaearon">Gaearon</Link> user profile
+    </div>
+  );
 };
 
 export default MainPage;
